4ed854a6eb1bbef5a4f41e3f377842265615a3c2
Sneha <snehaghosh12082002@gmail.com>
2023-05-12 18:32:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/detecto/MsgModel.java
@@ -0,0 +1,18 @@
+package com.example.detecto;
+
+public class MsgModel {
+
+    private String cnt;
+
+    public String getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(String cnt) {
+        this.cnt = cnt;
+    }
+
+    public MsgModel(String cnt) {
+        this.cnt = cnt;
+    }
+}
---
+++ app/src/main/java/com/example/detecto/RetrofitAPI.java
@@ -0,0 +1,11 @@
+package com.example.detecto;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+
+public interface RetrofitAPI {
+
+    @GET
+    Call<MsgModel> getMessage(@Url String url);
+}
