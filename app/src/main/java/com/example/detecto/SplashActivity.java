c3bfe89ab49ce210c78bc4067a20991aba1de0e2
Atmakendric <bapanmandal86958@gmail.com>
2023-05-13 14:01:50 +0530

    splash screen activity

--- app/src/main/java/com/example/detecto/SplashActivity.java
+++ app/src/main/java/com/example/detecto/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.example.detecto;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iHome=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(iHome);
+                finish();
+            }
+        },2000);
     }
 }
